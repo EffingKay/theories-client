ee8df8e564200b722b8e56c156681e71f496085b
EffingKay <klaudia@rozgonyiova.com>
2018-07-07 20:59:08 +0100

    changing config

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,1 @@
-// export const HOST_API = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://gotheory-api.herokuapp.com';
-
-export const HOST_API = 'http://localhost:8080';
+export const HOST_API = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://gotheory-api.herokuapp.com';
